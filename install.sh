da8fe8b057b1240c944d84a5b7b9e6c9f97b9480
Andrea Fioraldi <andreafioraldi@gmail.com>
2017-05-26 11:58:30 +0200

    Added exploitdb_mapping folder

--- install.sh
+++ install.sh
@@ -6,5 +6,3 @@ cp nmap_conf_prober /usr/bin/
 cp generated/cve_exploitdb_dict.py /usr/bin/
 cp cve_searchsploit.py /usr/bin/
 cp cve_searchsploit /usr/bin/
-cp search_from_nessus.py /usr/bin/
-cp search_from_nessus /usr/bin/
